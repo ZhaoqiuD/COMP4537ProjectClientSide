22b22103169896fe6ff334fedc40951259740081
ZhaoqiuD <dingzq0807@gmail.com>
2025-11-02 17:51:32 -0800

    Changed API url

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 // Client-side configuration for API base
 export const Config = Object.freeze({
   // Set this to your backend API origin (no trailing slash)
-apiBaseUrl: 'https://comp4537serverside-bcfgfrhxash8fkhv.canadacentral-01.azurewebsites.net/'
+apiBaseUrl: 'https://comp4537serverside-bcfgfrhxash8fkhv.canadacentral-01.azurewebsites.net/api'
   // apiBaseUrl: 'http://localhost:3000/api'
 });
 // moved to client/
