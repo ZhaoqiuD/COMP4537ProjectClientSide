c686450f25ef438536f06d793e62d1597db88d99
ZhaoqiuD <dingzq0807@gmail.com>
2025-11-13 16:15:50 -0800

    test cors

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,11 +1,13 @@
 // Client-side configuration for API base
 export const Config = Object.freeze({
   // Set this to your backend API origin (no trailing slash)
-// apiBaseUrl: 'https://comp4537serverside-bcfgfrhxash8fkhv.canadacentral-01.azurewebsites.net/api'
+  // apiBaseUrl: 'https://comp4537serverside-bcfgfrhxash8fkhv.canadacentral-01.azurewebsites.net/api'
 
-// apiBaseUrl: 'https://comp4537projectserverside-hvbud2gngtatg7cg.canadacentral-01.azurewebsites.net/api'
+  // apiBaseUrl: 'https://comp4537projectserverside-hvbud2gngtatg7cg.canadacentral-01.azurewebsites.net/api'
 
-// apiBaseUrl: 'https://comp4537projectserverside-qzcq.onrender.com/api'
-  apiBaseUrl: 'https://comp4537-projectserverside-e4hpapemggghh9ba.canadacentral-01.azurewebsites.net/api'
+  // apiBaseUrl: 'https://comp4537projectserverside-qzcq.onrender.com/api'
+  apiBaseUrl: 'https://comp4537-projectserverside-e4hpapemggghh9ba.canadacentral-01.azurewebsites.net/api',
+  // apiBaseUrl: "http://127.0.0.1:3000/api",
+ apiBaseUrl: "https://lovely-mooncake-c360ff.netlify.app/api"
 });
 // moved to client/
